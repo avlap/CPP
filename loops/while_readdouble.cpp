298758a00d3d8435582eaf6700f254b196cbe12b
debian <debian@debian-T4>
2021-02-26 13:25:58 +0100

    almost equal

--- loops/while_readdouble.cpp
+++ loops/while_readdouble.cpp
@@ -4,17 +4,26 @@ int main ()
 {
 	double val0;
 	double val1;
+	double small;
+	double large;
 	//int temp;
 	//int i = 0;
 	//cout << "Please enter 2 values: \n";
 	while(cin >> val0 >> val1) {
 		//cin >> val0 >> val1;
 		if(val0 > val1) { 
-			cout << "The smaller value is: " << val1 << " The larger value is: " << val0 << '\n';
+			small = val1;
+			large = val0;
+			cout << "The smaller value is: " << small << " The larger value is: " << large << '\n';
 		} else if (val0 < val1) {
-				cout << "The smaller value is: " << val0 << " The larger value is: " << val1 << '\n';
+				small = val0;
+				large = val1;
+				cout << "The smaller value is: " << small << " The larger value is: " << large << '\n';
 		} else
 			cout << "Both values are equal\n";
+		
+		if (large - small <= 1.0/100) 
+			cout << "The values are almost equal\n";
 
 	//	++i;
 	}
