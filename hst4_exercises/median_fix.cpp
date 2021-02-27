1891b98392c8ccc2a61ac73fa85492754cf09f1c
debian <debian@debian-T4>
2021-02-27 10:20:12 +0100

    median fixx

--- hst4_exercises/median_fix.cpp
+++ hst4_exercises/median_fix.cpp
@@ -12,14 +12,14 @@ int main ()
 	cout << "Average temperature: " << sum/temps.size() << '\n';
 
 	sort(temps);
-	if (temps.size % 2 == 0) 
+	if (temps.size() % 2 != 0) 
 		cout << "Median temperature: " << temps[temps.size()/2] << '\n';
 	else {
-		double median = (temps[temps.size()/2] + temps[(temps.size()/2) + 1])/2.0;
+		double median = (temps[temps.size()/2] + temps[(temps.size()/2) - 1])/2.0;
 		cout << "Median temperature: " << median << '\n';
 	}
 	
-//	cout << "Temps size: " << temps.size()/2 << '\n';
+	cout << "Temps size: " << temps.size()/2 << '\n';
 }
 
  
