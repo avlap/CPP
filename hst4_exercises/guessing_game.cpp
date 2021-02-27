2388d4ef64a7d65cf91c7fca9239368fb65c8164
debian <debian@debian-T4>
2021-02-27 12:36:37 +0100

    amai guessing

--- hst4_exercises/guessing_game.cpp
+++ hst4_exercises/guessing_game.cpp
@@ -52,17 +52,264 @@ int main ()
 						out_number = 91;
 				else
 					out_number = 94;
-
 			else if (in_number < 87)
 				out_number = 76;
+				if (in_number > 81)
+					out_number = 82;
+					if (in_number > 84)
+						out_number = 85;
+						if (in_number > 86)
+							out_number = 87;
+						else if (in_number < 86)
+							out_number = 85;
+						else
+							out_number = 86;
+					else if (in_number < 84)
+						out_number = 82;
+						if (in_number > 83)
+							out_number = 84;
+						else if (in_number < 83)
+							out_number = 82;
+						else
+							out_number = 83;
+					else 
+						out_number = 84;
+				else if (in_number < 81)
+					out_number = 76;
+					if (in_number > 79)
+						out_number = 80;
+						if (in_number > 80)
+							out_number = 81;
+						else 
+							out_number = 80;
+					else if (in_number < 79)
+						out_number = 76;
+						if (in_number > 77)
+							out_number = 78;
+							if (in_number 78)
+								out_number = 79;
+							else
+								out_number = 78;
+						else if (in_number < 77)
+							out_number = 76;
+						else
+							out_number = 77;
+					else
+						out_number = 79;
+				else 
+					out_number = 81;
 			else
 				out_number = 87;
 		else if (in_number < 75)
 			out_number = 51;
+			if (in_number > 58)
+				out_number = 59;
+				if (in_number > 72)
+					out_number = 73;
+					if (in_number > 74)
+						out_number = 75;
+					else if (in_number < 74)
+						out_number = 73;
+					else
+						out_number = 74;
+				else if (in_number < 72)
+					out_number = 59;
+					if (in _number > 66)
+						out_number = 67;
+						if (in_number > 69)
+							out_number = 70;
+							if (in_number > 70)
+								out_number = 71;
+							else
+								out_number = 70;
+						else if (in_number < 69)
+							out_number = 67;
+							if (in_number > 67)
+								out_number = 68;
+							else if (in_number < 67)
+								out_number = 66;
+							else
+								out_number = 67;
+						else
+							out_number = 69;
+					else if (in_number < 66)
+						out_number = 59;
+						if (in_number > 63)
+							out_number = 64;
+							if (in_number > 65)
+								out_number = 66;
+							else if (in_number < 65)
+								out_number = 64;
+							else
+								out_number = 65;
+						else if (in_number < 63)
+							out_number = 59;
+							if (in_number > 61)
+								out_number = 62;
+							else if (in_number < 61)
+								out_number = 59;
+								if (in_number > 59)
+									out_number = 60;
+								else 
+									out_number = 59;
+							else 
+								out_number = 61;
+						else 
+							out_number = 63;
+					else
+						out_number = 66;
+				else 
+					out_number = 72;
+			else if (in_number < 58)
+				out_number = 51;
+				if (in_number > 54)
+					out_number = 55;
+					if (in_number > 56)
+						out_number = 57;
+					else if (in_number < 56)
+						out_number = 55;
+					else
+						out_number = 56;
+				else if (in_number < 54)
+					out_number = 51;
+				else
+					out_number = 54;
+			else 
+				out_number = 58;
 		else
 			out_number = 75;
 	else if (in_number < 50)
 		out_number = 1;
+		if (in_number > 25)
+			out_number = 26;
+			if (in_number > 38)
+				out_number = 39;
+				if (in_number > 44)
+					out_number = 46;
+					if (in_number > 48)
+						out_number = 49;
+					else if (in_number < 48)
+						out_number = 47;
+						if (in_number > 46)
+							out_number = 47;
+						else if (in_number < 46)
+							out_number = 45;
+						else
+							out_number = 46;
+					else
+						out_number = 48;
+				else if (in_number < 44)
+					out_number = 39;
+					if (in_number > 41)
+						out_number = 42;
+						if (in_number > 43)
+							out_number = 44
+						else if (in_number < 43)
+							out_number = 42;
+						else
+							out_number = 43;
+					else if (in_number < 41)
+						out_number = 39;
+						if (in_number > 39)
+							out_number = 40;
+						else
+							out_number = 39;
+					else
+						out_number = 41;
+				else
+					out_number = 44;
+			else if (in_number < 38)
+				out_number = 26;
+				if (in_number > 32)
+					out_number = 33;
+					if (in_number > 35)
+						out_number = 36;
+						if (in_number > 37)
+							out_number = 38;
+						else if (in_number < 37)
+							out_number = 36;
+						else
+							out_number = 37;
+					else if (in_number < 35)
+						out_number = 33;
+						if (in_number > 33)
+							out_number = 34;
+						else
+							out_number = 33;
+					else
+						out_number = 35;
+				else if (in_number < 32)
+					out_number = 26;
+					if (in_number > 29)
+						out_number = 30;
+						if (in_number > 31)
+							out_number = 32;
+						else if (in_number < 31)
+							out_number = 30;
+						else
+							out_number = 31;
+					else if (in_number < 29)
+						out_number = 26;
+						if (in_number > 27)
+							out_number = 28;
+						else if (in_number < 27)
+							out_number = 26;
+						else
+							out_number = 27;
+					else
+						out_number = 29;
+				else
+					out_number = 32;
+			else
+				out_number = 38;
+		else if (in_number < 25)
+			out_number = 1;
+			if (in_number > 12)
+				out_number = 13;
+				if (in_number > 19)
+					out_number = 20;
+					if (in_number > 22)
+						out_number = 23;
+						if (in_number > 23)
+							out_number = 24;
+						else 
+							out_number = 23;
+					else if (in_number < 22)
+						out_number = 20;
+						if (in_number > 20)
+							out_number = 21;
+						else
+							out_number = 20;
+					else
+						out_number = 22;
+				else if (in_number < 19)
+					out_number = 13;
+					if (in_number > 16)
+						out_number = 17;
+						if (in_number > 18)
+							out_number = 19;
+						else if (in_number < 18)
+							out_number = 17;
+						else
+							out_number = 18;
+					else if (in_number < 16)
+						out_number = 13;
+						if (in_number > 14)
+							out_number = 15;
+						else if (in_number < 14)
+							out_number = 13;
+						else
+							out_number = 14;
+					else
+						out_number = 16;
+				else
+					out_number = 19;
+			else if (in_number < 12)
+				out_number = 1;
+			else
+				out_number = 12;
+		else
+			out_number = 25;
 	else
 		out_number = 50;
 
