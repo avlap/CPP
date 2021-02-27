6f77622dbe3f29437d733817a109635fda7576f4
debian <debian@debian-T4>
2021-02-27 17:32:44 +0100

    reverse

---
+++ hst4_exercises/string_vector.cpp
@@ -0,0 +1,26 @@
+
+
+#include "../include/std_lib_facilities.h"
+
+int main ()
+{
+	int val0;
+	string val1;
+	vector<string>values = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+
+	cin >> val1;
+
+if (val1 == values[])
+
+
+
+
+
+	
+	cout << val1 << '\n';
+
+//	for(int x:values) ++1;
+
+}
+
+
