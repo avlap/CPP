eef97d455f768fcf8682ee4650730670da181ec4
debian <debian@debian-T4>
2021-02-27 12:52:01 +0100

    without brackets

--- hst4_exercises/guessing_game.cpp
+++ hst4_exercises/guessing_game.cpp
@@ -8,84 +8,56 @@ int main ()
 	int out_number;
 	cout "Please Enter a number between 1 and 100: \n";
 	cin >> in_number;
-	if (in_number > 50){
+	if (in_number > 50)
 		out_number = 51;
-	}
-	else if (in_number < 50)
-		out_number = 1;
-
-		if (in_number > 75) {
+		if (in_number > 75)
 			out_number = 76;
-		}
-			if (in_number > 87){
+			if (in_number > 87)
 				out_number = 88;
-			}
-				if (in_number > 94) {
+				if (in_number > 94)
 					out_number = 95;
-				}
-					if (in_number > 97) {
+					if (in_number > 97)
 						out_number = 98;
-					}
-						if (in_number > 99){
+						if (in_number > 99)
 							out_number = 100;
-						}
-						else if (in_number < 99){
+						else if (in_number < 99)
 							out_number = 98;
-						}
-						else {
+						else
 							out_number = 99; 
-						}
-					else if (in_number < 97) {
+					else if (in_number < 97)
 						out_number = 95;
-					}
-						if (in_number > 95){
+						if (in_number > 95)
 							out_number = 96;
-						}
-						else {
+						else
 							out_number = 95;
-						}
-					else {
+					else 
 						out_number = 97;
-					}
-				else if (in_number < 94) {
+				else if (in_number < 94)
 					out_number = 88;
-				}
-					if (in_number > 91){
+					if (in_number > 91)
 						out_number = 92;
-					}
-						if (in_number > 92) {
+						if (in_number > 92)
 							out_number = 93;
-						}
-						else{
+						else
 							out_number = 92;
-						}
-					else if (in_number < 91){
+					else if (in_number < 91)
 						out_number = 88;
-					}
-						if (in_number > 89){
+						if (in_number > 89)
 							out_number = 90;
-						}
-						else if (in_number < 89){
+						else if (in_number < 89)
 							out_number = 88;
-						}
-						else{
+						else
 							out_number = 89;
-						}
-					else {
+					else
 						out_number = 91;
-					}
-				else {
+				else
 					out_number = 94;
-				}
-			else if (in_number < 87) {
+			else if (in_number < 87)
 				out_number = 76;
-			}
-				if (in_number > 81){
+				if (in_number > 81)
 					out_number = 82;
-				}
-					if (in_number > 84) {
+					if (in_number > 84)
 						out_number = 85;
-					}
 						if (in_number > 86)
 							out_number = 87;
 						else if (in_number < 86)
@@ -206,8 +178,8 @@ int main ()
 				out_number = 58;
 		else
 			out_number = 75;
-
-
+	else if (in_number < 50)
+		out_number = 1;
 		if (in_number > 25)
 			out_number = 26;
 			if (in_number > 38)
@@ -372,11 +344,6 @@ int main ()
 			out_number = 25;
 	else
 		out_number = 50;
-
-
-
-
-
 
 
 }
