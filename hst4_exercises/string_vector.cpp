da654a78be44d52a89fc3d86c682ee5df9bb7734
debian <debian@debian-T4>
2021-02-27 21:07:02 +0100

    solution?

--- hst4_exercises/string_vector.cpp
+++ hst4_exercises/string_vector.cpp
@@ -4,36 +4,42 @@
 
 int main ()
 {
-	int val0;
-	string val1;
-	int output0;
-	//int output1;
-	vector<string>values = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+	//int val0;
+	string val0;
+	string output0;
+	string output1;
+	vector<string>values0 = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+	vector<string>values1 = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
 
-	cin >> val1;
+	cin >> val0;
 
-	for (int i = 0; i < values.size(); ++i) {
-		if (val1 == values[i]) {
-			output0 = i;
-		} 
+
+	for (int i =0; i < values1.size(); ++i){
+		if(val0 == values1[i])
+			output0 = values0[i];
 	}
+
+	for (int i =0; i < values0.size(); ++i){
+		if(val0 == values0[i])
+			output0 = values1[i];
+	}
+
+
+cout << output0 << '\n';
+
+
+	
 	
 
-//	for (int i =0; i <=10; ++i) {
-//		if (val1 == "i") {
-//			output0 = i;
-//		}
-//		++i;
-//}
+	
 
 
 
 
 
 
-
-	cout << output0 << '\n';
-	cout << values.size() << '\n';
+	//cout << output0 << '\n';
+		//cout << "val1: " << val1 << '\n';
 
 	//	for(int x:values) ++1;
 
---
+++ hst4_exercises/string_vector2.cpp
@@ -0,0 +1,17 @@
+
+
+#include "../include/std_lib_facilities.h"
+
+int main ()
+{
+	int val0;
+	string val1;
+	int output0;
+	string output1;
+	vector<string>values = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+
+	cin >> val1;
+
+}
+
+
