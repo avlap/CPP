1d829c27255a43f4a586b7e26fa5455727b2e3a9
debian <debian@debian-T4>
2021-02-28 12:39:58 +0100

    chess

---
+++ hst4_exercises/3string_vector.cpp
@@ -0,0 +1,32 @@
+
+
+#include "../include/std_lib_facilities.h"
+
+int main ()
+{
+	//int val0;
+	string val0;
+	string output0;
+	//string output1;
+	vector<string>values0 = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+	vector<string>values1 = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+
+	cin >> val0;
+
+
+	for (int x : values1){
+		if(val0 == values1[x])
+			output0 = values0[x];
+	}
+
+	for (int i =0; i < values0.size(); ++i){
+		if(val0 == values0[i])
+			output0 = values1[i];
+	}
+
+
+cout << output0 << '\n';
+
+}
+
+
---
+++ hst4_exercises/chess.cpp
@@ -0,0 +1,21 @@
+#include "../include/std_lib_facilities.h"
+
+int main ()
+{
+	int grain = 1;
+	int totalgrain = 0;
+	for(int square = 1; square <= 64;  ++square) {
+		if (square > 1){
+			grain = grain * 2;
+			totalgrain += grain;
+			cout << "square: " << square << '\t' << "grain: " << grain << '\t' << '\t' << "totalgrain: " << totalgrain << '\n';
+		} else {
+			totalgrain += grain;
+			cout << "square: " << square << '\t' << "grain: " << grain << '\t' << '\t' << "totalgrain: " << totalgrain << '\n';
+		}
+
+	}
+
+}
+
+
