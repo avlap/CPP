38a5e1cb92513d6e5d2afe38d3f27cc93a838f6d
debian <debian@debian-T4>
2021-03-01 17:45:08 +0100

    mode

---
+++ loops/mode.cpp
@@ -0,0 +1,45 @@
+#include "../include/std_lib_facilities.h"
+
+int main ()
+{
+	double val;
+	//double val1;
+	double small;
+	double large;
+	vector<int>numbers;
+	int max_mode = 0;
+	int temp_mode = 0;
+	int mode = 0;
+	//	char unit = ' ';
+	//int temp;
+	//int i = 0;
+	//cout << "Please enter 2 values: \n";
+	while(cin >> val) {
+		//cin >> val0 >> val1;
+		
+		for (int i =0; i < numbers.size(); ++i) {
+			if(val == numbers[i])
+				++temp_mode;
+		}
+
+		if(temp_mode > max_mode) {
+			mode = val;
+			max_mode = temp_mode;
+		}
+	
+		numbers.push_back(val);
+
+
+	}
+
+	cout << "Numbers: \n";
+	for (int i =0; i < numbers.size(); ++i) {
+		cout <<  numbers[i] << '\n';
+	}
+	cout << "Mode: " << mode << '\n';
+		
+
+
+}
+
+
