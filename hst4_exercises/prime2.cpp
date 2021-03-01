668672e5da6e8714511f248d14e673c668f0aef3
debian <debian@debian-T4>
2021-03-01 13:00:51 +0100

    clean up

--- hst4_exercises/prime2.cpp
+++ hst4_exercises/prime2.cpp
@@ -2,25 +2,23 @@
 
 int main ()
 {
-	vector<int>primes = {2, 3};
+	vector<int>primes = {2};
 	int n;
 	bool prime = 0;
 
-	for(int i=0; i < 100; ++i) {
-		if (i > 3) {
-			for (int j =0; j < primes.size(); ++j) {
-				if (i % primes[j] == 0) {
-					prime = 1;	
-				}
+	for(int i=3; i < 100; ++i) {
+		for(int j =0; j < primes.size(); ++j) {
+			if(i % primes[j] == 0) {
+				prime = 1;	
 			}
-			if (prime == 0) {
-				primes.push_back(i);
-			} else
-				prime = 0;
 		}
+		if(prime == 0) {
+			primes.push_back(i);
+		} else
+			prime = 0;
 	}
 
-	for (int i=0; i < primes.size(); ++i) {
+	for(int i=0; i < primes.size(); ++i) {
 		cout << primes[i] << '\n';
 	}
 }
