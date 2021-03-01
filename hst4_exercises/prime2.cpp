1e739d5c2b759d7df90bb2a4a699f15ceb4cede5
debian <debian@debian-T4>
2021-03-01 12:57:04 +0100

    bool prime

--- hst4_exercises/prime2.cpp
+++ hst4_exercises/prime2.cpp
@@ -4,7 +4,7 @@ int main ()
 {
 	vector<int>primes = {2, 3};
 	int n;
-	int prime = 0;
+	bool prime = 0;
 
 	for(int i=0; i < 100; ++i) {
 		if (i > 3) {
@@ -12,13 +12,11 @@ int main ()
 				if (i % primes[j] == 0) {
 					prime = 1;	
 				}
-				//write a function to check prime number
 			}
 			if (prime == 0) {
 				primes.push_back(i);
 			} else
 				prime = 0;
-
 		}
 	}
 
