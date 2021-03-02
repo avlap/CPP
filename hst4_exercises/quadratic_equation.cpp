0fc1275cb79b8318786d11df6f7a8ed8a83ec293
debian <debian@debian-T4>
2021-03-02 12:11:12 +0100

    I give up on this one

--- hst4_exercises/quadratic_equation.cpp
+++ hst4_exercises/quadratic_equation.cpp
@@ -13,7 +13,8 @@ int main ()
 	double c_val2;
 
 	double r_val=0.0;
-	double l_val;
+	double val_3;
+	
 
 
 	cout <<  "ax² + bx + c = 0\n";
@@ -22,9 +23,9 @@ int main ()
 	cout << a_val << "x² + " << b_val << "x + " << c_val << " = 0\n";
 
 	cout << "1. Divide each side by a\n";
-	b_val1 = b_val/a_val;
-	c_val1 = c_val /a_val;
-	a_val1 = a_val/a_val;
+	b_val1 = b_val / a_val;
+	c_val1 = c_val / a_val;
+	a_val1 = a_val / a_val;
 
 //	r_val = r_val + abs(c_val1);
 
@@ -40,11 +41,17 @@ int main ()
 
 	cout << a_val1 << "x² + " << b_val1 << "x + " << c_val2 << " = " << r_val << '\n';
 
-	//Add the square of one-half of b/a, the coefficient of x, to both sides. This "completes the square", converting the left side into a perfect square.
-//	val_3 = 0.5 * (b_val1);
-//	val_3 = val_3 * val3;
+	cout << "3. Add the square of one-half of b/a, the coefficient of x, to both sides. This completes the square, converting the left side into a perfect square.\n";
 
+	val_3 = 0.5 * (b_val1);
+	val_3 = val_3 * val_3;
 
+	r_val = r_val + val_3;
+	c_val2 = c_val2 + val_3;
+	
+	cout << a_val1 << "x² + " << b_val1 << "x + " << c_val2 << " = " << r_val << '\n';
+
+	cout << "5. Produce two linear equations by equating the square root of the left side with the positive and negative square roots of the right side.\n";
 }
 
  
