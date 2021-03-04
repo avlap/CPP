0bc0083c967885a038f2c5c672f52d46eb9f59fe
debian <debian@debian-T4>
2021-03-04 15:04:47 +0100

    units

--- hst5/kelvin.cpp
+++ hst5/kelvin.cpp
@@ -7,17 +7,31 @@ double ctook(double c) //converts celcius to Kelvin
 	return k; 
 }
 
+double kelvin_to_c(double k) //converts kelvin to celcius
+{
+	double ck = k - 273.15;
+	return ck;
+
+}
+
 
 int main ()
 {
 
+	char unit = ' ';	
 	double c;
-	cin >> c;
-	if (c < -273.15) error("Value can't be lower then -273.15C\n");
+	cin >> c >> unit;
+	if(unit == 'c')
+		if (c < -273.15) error("Value can't be lower then -273.15C\n");
+		double k = ctook(c);
+		cout << "Kelvin: " << k <<'\n';
+	else if(unit == 'k')
+		double ck = kelvin_to_c(c);
+		cout << "Celcius: " << ck <<'\n';
+	else
+		cout << "Error: unknown unit!\n";
 
-	double k = ctook(c);
-	cout << "Kelvin: " << k <<'\n';
-
+	
 }
 
 
