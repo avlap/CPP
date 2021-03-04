744509994343236b9718fb96011f46b3767af80b
debian <debian@debian-T4>
2021-03-04 23:19:02 +0100

    still those integers

--- hst5/integers.cpp
+++ hst5/integers.cpp
@@ -1,24 +1,27 @@
 
 #include "../include/std_lib_facilities.h"
 
-void num_int(int x, int y)
+void sum_integers(int x, int y)
 {
+	//funtion for calculating sum from entered integers
+	//exercises chapter 5
+	
 	cout << "Please enter the number of values you want to sum: \n";
-	cin >> x;
+	cin >> x; //enter number from console
 
 	vector<int>integers;
 	cout << "Please enter some integers (press '|' to stop): \n";
 	for(y; cin >> y;)
-		integers.push_back(y);
+		integers.push_back(y); //add integers to a vector
 
-	if(x>integers.size()) error("You didn't enter enough integers!\n");
+	if(x>integers.size()) error("You didn't enter enough integers!\n"); //if there are not enough integers, error
 
 	int sum = 0;
-	for(int i =0; i<x; ++i)
-		sum +=integers[i];
+	for(int i =0; i<x; ++i) 
+		sum +=integers[i]; //calculate the sum
 
 
-	cout << "The sum of the first " << x << " numbers is: " << sum << '\n';
+	cout << "The sum of the first " << x << " numbers is: " << sum << '\n'; //print out the sum
 
 }
 
@@ -26,7 +29,7 @@ void num_int(int x, int y)
 int main()
 {
 	int x, y;
- 	num_int(x, y);
+ 	sum_integers(x, y);
 
 }
 
