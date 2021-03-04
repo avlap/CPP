bdd93fd606b8c3b2044f8d30dde28129c27bb3a8
debian <debian@debian-T4>
2021-03-04 15:08:42 +0100

    block fix

--- hst5/kelvin.cpp
+++ hst5/kelvin.cpp
@@ -21,14 +21,14 @@ int main ()
 	char unit = ' ';	
 	double c;
 	cin >> c >> unit;
-	if(unit == 'c')
+	if(unit == 'c') {
 		if (c < -273.15) error("Value can't be lower then -273.15C\n");
 		double k = ctook(c);
 		cout << "Kelvin: " << k <<'\n';
-	else if(unit == 'k')
+	} else if(unit == 'k') {
 		double ck = kelvin_to_c(c);
 		cout << "Celcius: " << ck <<'\n';
-	else
+	} else
 		cout << "Error: unknown unit!\n";
 
 	
