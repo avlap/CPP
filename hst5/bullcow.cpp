ca41f2b121abbb5a38817f3cc1ffa981e3dfcc86
debian <debian@debian-T4>
2021-03-05 12:15:22 +0100

    error checks

--- hst5/bullcow.cpp
+++ hst5/bullcow.cpp
@@ -6,19 +6,22 @@ void bullcow()
 
 	vector<int>numbers = {5,2,7,1};
 	vector<int>guess_numbers;
-	//int g;
 
 	cout << numbers.size() << '\n';
 
-	cout << "Enter 4 numbers please: \n";
-	//for(int i =0; i < guess_numbers.size(); ++i) 
-	//for(int i = 0; i < 4; ++i)
-	for(int g; cin >> g;)
-		guess_numbers.push_back(g);
+	cout << "Enter 4 different numbers please, followed with a '|' sign: \n";
 
-	if(guess_numbers.size() -1 > numbers.size()) error("You entered more then 4 values!\n");
-	//why -1?
-//	cout << guess_numbers.size() << '\n';
+	for(int g; cin >> g;) {
+		if(g < 0 || g > 9) error("Only numbers between 0 - 9 please!\n");
+		guess_numbers.push_back(g);
+	}
+	
+
+	if(guess_numbers.size()  > numbers.size()) error("You entered more then 4 values!\n");
+
+	for(int i=1; i < guess_numbers.size(); ++i)
+		for(int x:guess_numbers)
+			if(guess_numbers[i] == guess_numbers[x]) error("You should not enter the same value twice!\n");
 
 	if(numbers == guess_numbers)
 		cout << "Congrats!\n";
