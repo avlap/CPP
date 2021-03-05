b1a21c124cb818031a487cd9fa35916bfe8bf2c7
debian <debian@debian-T4>
2021-03-05 11:41:13 +0100

    check

--- hst5/bullcow.cpp
+++ hst5/bullcow.cpp
@@ -5,13 +5,15 @@ void bullcow()
 {
 
 	vector<int>numbers = {5,2,7,1};
-
-	int g;
 	vector<int>guess_numbers(4);
+	int g;
+
 	cout << "Enter 4 numbers please: \n";
 	//for(int i =0; i < guess_numbers.size(); ++i) 
 	for(int x:guess_numbers)
 		cin >> g; guess_numbers.push_back(g);
+
+	if(guess_numbers.size() > numbers.size()) error("You entered more then 4 values!\n");
 
 }
 
