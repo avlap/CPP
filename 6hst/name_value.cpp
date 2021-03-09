8a1e03fe4522b1ca14c42065f809e3911099762c
debian <debian@debian-T4>
2021-03-09 10:43:21 +0100

    cleanup

--- 6hst/name_value.cpp
+++ 6hst/name_value.cpp
@@ -17,15 +17,13 @@ int main()
 	//Name_value n = {"henk", 7};
 	vector<Name_value> v = {{"henk", 7}, {"piet", 8}, {"Jo", 27}, {"Kees", 28}};
 
+	//compare names and check if unique
 	for(int i =0; i < v.size(); ++i) {
 		for(int j=0; j<v.size(); ++j) {
 			if(i != j) 
 				if(v[i].name == v[j].name) error("Same name");
 		}
 	}
-
-	//compare names and check if unique
-	//
 
 	//write out all the pairs on a line
 	for(int i=0; i<v.size(); ++i)
