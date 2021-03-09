7b6981c805161cfa785a7a7360ca46facead7fe4
debian <debian@debian-T4>
2021-03-09 13:24:35 +0100

    add char to string

--- 6hst/good_sentence.cpp
+++ 6hst/good_sentence.cpp
@@ -3,13 +3,17 @@
 int main()
 {
 	//read words into a string vector
-	vector<string> s;
-	for(string tmp_s; cin >>tmp_s;) {
-		if(tmp_s == "fish") error("fish"); 
+	//vector<string> s;
+	string s;
+	for(char tmp_s; cin >>tmp_s;) 
 		s.push_back(tmp_s);
-	}
+	//}
+	//
+	//second test
+	//string s;
+	//cin >> s;
 
 	//print out content of s
-	for(string x:s)
-		cout << x << '\n';
+	//for(string x:s)
+		cout << s << '\n';
 }
