362dcce67b4645c29501ead9b73903ab72ea872e
debian <debian@debian-T4>
2021-03-09 13:57:17 +0100

    sentence

--- 6hst/good_sentence.cpp
+++ 6hst/good_sentence.cpp
@@ -1,19 +1,32 @@
 #include "../include/std_lib_facilities.h"
 
+
+
 int main()
 {
+	//bool = is_ok = false;
 	//read words into a string vector
-	//vector<string> s;
-	string s;
-	for(char tmp_s; cin >>tmp_s;) 
+	vector<string> s;
+	for(string tmp_s; cin >>tmp_s;) 
 		s.push_back(tmp_s);
-	//}
-	//
-	//second test
-	//string s;
-	//cin >> s;
+
+	for(string x:s)
+		cout << x << ' ';
+	
+	cout << '\n';
+
+//	if(s[0] != noun || s[0] != lidwoord)
+//		is_ok = false;
+//	else
+//		if(s[1] == noun
+	
 
 	//print out content of s
 	//for(string x:s)
-		cout << s << '\n';
+//	cout << s << '\n';
+	
+	//return 0;
+	
 }
+
+//The birds fly and fish swim .
