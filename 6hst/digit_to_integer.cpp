4e49f725853b247f821acab7cc822d7a42bf0cf4
debian <debian@debian-T4>
2021-03-09 22:16:16 +0100

    class

--- 6hst/digit_to_integer.cpp
+++ 6hst/digit_to_integer.cpp
@@ -1,22 +1,35 @@
 #include "../include/std_lib_facilities.h"
 
+class Digit {
+	public:
+		char digit; 
+		int number;
+};
+
 int main()
 {
 
 	char nul = '0';
+	int amount = 0;
 
-	vector<int>digit;
+	vector<Digit>digit;
 	for(char tmp_digit; cin >> tmp_digit;) {
+		++amount;
 		tmp_digit -=nul;
-		digit.push_back(tmp_digit);
+		digit.push_back({tmp_digit, amount});
 	}
 
-		for(char x:digit)
-		x -=nul;
+		//for(char x:digit)
+		//x -=nul;
 
-	for(int x:digit)
-		cout << x;
+	//for(int x:digit)
+	//	cout << x;
+
+	//for(int i= amount; i >= 0; --i) 
+	//cout << 
 	
-	cout << '\n';
+	//cout << '\n';
+
+	//cout << amount << '\n';
 
 }
