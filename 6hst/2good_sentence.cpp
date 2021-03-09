a55467faa26b3562f8d77caff44c32ad451654f6
debian <debian@debian-T4>
2021-03-09 14:26:22 +0100

    attempt

--- 6hst/2good_sentence.cpp
+++ 6hst/2good_sentence.cpp
@@ -11,6 +11,34 @@ int main()
 	for(string tmp_s; cin >>tmp_s;) 
 		s.push_back(tmp_s);
 
+	for(int i=0; i < s.size(); ++i) {
+		switch(s[i]) {
+			case 'the':
+				if(s[i+1] = is_noun()) {
+					sentence = ok;
+				else
+					sentence != ok;
+				}
+				break;
+			case 'fly': 'swim':
+					if (s[i-1] != is_noun()) {
+						sentence = ok;
+					else
+						sentence != ok;
+					}
+				break;
+			case 'bird': 'fish':
+					if (s[i+1] != is_verb()) {
+						sentence != ok;
+					else
+						sentence = ok;
+					}
+				break;
+			case 'and': 'or': 'but':
+						if (s[i +1] == is_sentence())
+
+		}
+	}
 
 
 	for(string x:s)
