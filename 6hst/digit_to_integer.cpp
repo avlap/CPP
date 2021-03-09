b7f284c27b888b308fda6ddbe80950f6fbb0ebfc
debian <debian@debian-T4>
2021-03-09 21:59:04 +0100

    transfer to integer

--- 6hst/digit_to_integer.cpp
+++ 6hst/digit_to_integer.cpp
@@ -2,11 +2,19 @@
 
 int main()
 {
-	vector<char>digit;
-	for(char tmp_digit; cin >> tmp_digit;)
-		digit.push_back(tmp_digit);
 
-	for(char x:digit)
+	char nul = '0';
+
+	vector<int>digit;
+	for(char tmp_digit; cin >> tmp_digit;) {
+		tmp_digit -=nul;
+		digit.push_back(tmp_digit);
+	}
+
+		for(char x:digit)
+		x -=nul;
+
+	for(int x:digit)
 		cout << x;
 	
 	cout << '\n';
