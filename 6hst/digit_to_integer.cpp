72074ccdb512de5b10e8217aeb82b76ac1fab710
debian <debian@debian-T4>
2021-03-11 10:45:16 +0100

    improvements

--- 6hst/digit_to_integer.cpp
+++ 6hst/digit_to_integer.cpp
@@ -12,24 +12,60 @@ int main()
 	char nul = '0';
 	int amount = 0;
 
+	int zero = 0;
+	int one = 0;
+	int two = 0;
+	int three = 0; 
+	int four = 0;
+
+	int number;
+
 	vector<int>digit;
 	for(char tmp_digit; cin >> tmp_digit;) {
 		tmp_digit -=nul;
 		digit.push_back(tmp_digit);
 	}
 
+	for(int x:digit)
+		cout << x;
+
 	for(int i=0; i < digit.size(); ++i) {
 		switch(i) {
 			case 0:
-				int zero = digit[i];
+				zero = digit[digit.size()-1];
+				cout << " is " << zero << " zeros";
 				break;
-		
+			case 1:
+				one = digit[digit.size()-2];
+				cout << ", " << one << " tens";
+				break;
+			case 2:
+				two = digit[digit.size()-3];
+				break;
+			default:
+				error("No number entered");
+				break;
+
+
+
 		}
+			
+	}
+	//	if(i == digit.size()-1)
+	//zero = digit[digit.size() -1];
+		
+		
 
 
 		
 
-	}
+	
+
+	
+
+//	cout << ' ' << "is " << zero << " zeros " << one << " tens, ";
+
+	cout << '\n';
 
 		//for(char x:digit)
 		//x -=nul;
