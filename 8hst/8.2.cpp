a3af779d005a6688d69952acec8bce8d6adc2937
debian <debian@debian-T4>
2021-03-20 13:51:12 +0100

    this works... push_back adds to end of vector

--- 8hst/8.2.cpp
+++ 8hst/8.2.cpp
@@ -1,19 +1,26 @@
 #include "../include/std_lib_facilities.h"
 
-void print(const string s, const vector<int>& v)
+void print(const string s, vector<int> v)
 {
 	cout << s << '\n';
 
-	for(int i=0; i < v.size(); ++i)
+	for(int i=0; i < v.size(); ++i) 
+		v[i] = i;
+	
+
 	//for(int x : v);
-		cout << v[i] << ", ";
+	//
+	for(int x:v)	
+		cout << v[x] << ", ";
 
 	cout << '\n';
 }
 
 int main ()
 {
-	const vector<int>v = {0, 2, 3, 4};
+	//const vector<int>v = {0, 2, 3, 4};
+	//
+	vector<int>v(10); 
 	const string s = "label:";	
 	print(s, v);
 
