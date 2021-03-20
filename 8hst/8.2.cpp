602248f0aceeaa8e76ef1702e0952108e250c8c7
debian <debian@debian-T4>
2021-03-20 13:35:25 +0100

    const

--- 8hst/8.2.cpp
+++ 8hst/8.2.cpp
@@ -1,20 +1,20 @@
 #include "../include/std_lib_facilities.h"
 
-void print(const string& s, const vector<int>& v)
+void print(const string s, const vector<int>& v)
 {
 	cout << s << '\n';
 
 	for(int i=0; i < v.size(); ++i)
 	//for(int x : v);
-		cout << v[i] << " , ";
+		cout << v[i] << ", ";
 
 	cout << '\n';
 }
 
 int main ()
 {
-	vector<int>v = {0, 2, 3, 4};
-	string s = "label";	
+	const vector<int>v = {0, 2, 3, 4};
+	const string s = "label:";	
 	print(s, v);
 
 	return 0;
