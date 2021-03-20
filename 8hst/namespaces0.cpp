b3085873a0ea5b2f58da7b88c8bf5599b24f0965
debian <debian@debian-T4>
2021-03-20 10:50:53 +0100

    namespaces

---
+++ 8hst/namespaces0.cpp
@@ -0,0 +1,24 @@
+namespace X {
+	int var;
+
+	void print() {
+		cout << "var: " << var << '\n';
+	}
+
+}
+
+namespace Y {
+	int var;
+
+	void print() {
+		cout << "var: " << var << '\n';
+	}
+}
+
+namespace Z {
+	int var;
+
+	void print() {
+		cout << "var: " << var << '\n';
+	}
+}
--- 8hst/swap.cpp
+++ 8hst/swap.cpp
@@ -16,20 +16,23 @@ void swap_r(int& a, int& b)
 	b=temp;
 }
 
-
+void swap_cr(const int& a, const int& b)
+{
+	int temp;
+	temp=a;
+	a=b;
+	b=temp;
+}
 
 int main()
 {
-	
-
 	//swap
-	int x{7};
-	int y{9};
+	//int x{7};
+	//int y{9};
+	const int cx = 7;
+	const int cy = 9;
 
-	swap_r(x, y);
-	cout << "x: " << x << "y: " << y << '\n'; //pass by value, makes a copy
-
-	
-
+	swap_cr(cx, j);
+	//cout << "x: " << x << "y: " << y << '\n'; //pass by value, makes a copy
 	
 }
