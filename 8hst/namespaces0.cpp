6808ad896b61ad2350f23893cbb937ed1f9bbad0
debian <debian@debian-T4>
2021-03-20 10:55:26 +0100

    hst 8, exc 3

--- 8hst/namespaces0.cpp
+++ 8hst/namespaces0.cpp
@@ -1,3 +1,5 @@
+#include "../include/std_lib_facilities.h"
+
 namespace X {
 	int var;
 
@@ -21,4 +23,23 @@ namespace Z {
 	void print() {
 		cout << "var: " << var << '\n';
 	}
+}
+
+int main()
+{
+	X::var = 7;
+	X::print();
+	using namespace Y;
+	var = 9;
+	print()
+	{
+		using Z::var;
+		using Z::print;
+		var = 11;
+		print();
+	}
+
+	print();
+	X::print();
+
 }
