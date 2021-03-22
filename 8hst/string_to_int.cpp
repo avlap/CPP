eb8b2e34b8a9eb374e9ebdfed24ff86c195d42b9
debian <debian@debian-T4>
2021-03-22 17:01:06 +0100

    skip for now

---
+++ 8hst/string_to_int.cpp
@@ -0,0 +1,31 @@
+#include "../include/std_lib_facilities.h"
+
+//write function, takes a vector<string> returns a vector<int>, containing the numbers of characters in a string
+//
+//read each entry in the vector<string> as characters. (function?!)
+//make a vector of ints with the lengts of
+
+void num_char_in_string(vector<string> v)
+{
+
+	//for(string x:v)
+	string x = v[0];
+
+	for(int i=0; i
+		char c;
+		
+
+
+
+
+}
+
+
+
+int main ()
+{
+	vector<string> s = {"one", "two", "three", "zeven"};
+
+	num_char_in_string(s);
+
+}
