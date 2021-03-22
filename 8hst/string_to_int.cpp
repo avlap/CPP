032cf058868e6a4e0d5d5e12374c791baab539eb
debian <debian@debian-T4>
2021-03-22 17:14:56 +0100

    trying anyway

--- 8hst/string_to_int.cpp
+++ 8hst/string_to_int.cpp
@@ -4,15 +4,32 @@
 //
 //read each entry in the vector<string> as characters. (function?!)
 //make a vector of ints with the lengts of
+//
+
+vector<string> makestring()
+{
+
+	vector<string>v;
+
+	for(string temp; cin>> temp;){
+	    int x = temp.size();	
+		cout << x << '\n';
+		v.push_back(temp);
+	}
+
+	for(string x:v)
+		cout << x << '\n';
+
+
+}
 
 void num_char_in_string(vector<string> v)
 {
 
 	//for(string x:v)
 	string x = v[0];
+	char y;
 
-	for(int i=0; i
-		char c;
 		
 
 
@@ -24,8 +41,8 @@ void num_char_in_string(vector<string> v)
 
 int main ()
 {
-	vector<string> s = {"one", "two", "three", "zeven"};
+	//vector<string> s = {"one", "two", "three", "zeven"};
 
-	num_char_in_string(s);
-
+	//num_char_in_string(s);
+	makestring();
 }
