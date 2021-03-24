fc89010cec63cd2954efec087519182cde245689
debian <debian@debian-T4>
2021-03-24 10:49:46 +0100

    page 314

--- 9hst/notes.cpp
+++ 9hst/notes.cpp
@@ -4,13 +4,14 @@
 //	int d;
 //};
 class Date{
-	int y,m,d;
 public:
 	Date(int y, int m, int d);
 	void add_day(int n);
 	int month() {return m;}
 	int day() {return d;}
 	int year() {return y;}
+private:
+	int y,m,d;
 };
 		
 
