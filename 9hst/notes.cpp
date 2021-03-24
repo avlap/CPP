08c6e311d9c1bf7475bb9ef250ea49ab5865e88b
debian <debian@debian-T4>
2021-03-24 11:48:48 +0100

    delete some

--- 9hst/notes.cpp
+++ 9hst/notes.cpp
@@ -27,7 +27,7 @@ void Date::add_day(int n)
 
 class Date{
 	public: 
-		class Invalid{};
+		class Invalid{}; //to be used as exception
 		Date(int y, int m, int d);
 	private:
 		int y, int m, int d;
@@ -47,27 +47,4 @@ bool Date::is_valid()
 	if(m<1||12<m) return false;
 }
 
-//helper functions
-void init_day(Date& dd, int y, int m, int d)
-{
-	//chec that (y,m,d) is a valid date
-	//if it is, use it to initiate dd
-
-}
-
-void add_day(Date& dd, int n)
-{
-	//increase dd by n days
-	//
-}
-
-void f()
-{
-	Date today;
-	int_day(today, 12, 24, 2005); // oops! (no day 2005 in year 12)
-	add_day(today,1);
-}
-
-Date birthday {1970,12,30};
-cout 
 
