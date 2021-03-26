76eee0f0a8e6a783f9740a925f6e44788eade8cc
debian <debian@debian-T4>
2021-03-26 21:03:25 +0100

    clean up

--- 9hst/9.4.cpp
+++ 9hst/9.4.cpp
@@ -4,7 +4,10 @@ class Date{
 	public: 
 	
 	Date(int y, int m, int d);
-	void add_day(Date& dd, int n);
+	void add_day(int n);
+	int year() {return y;}
+	int month() {return m;}
+	int day() {return d;}
 
 	private:
 	int y, m, d;
@@ -17,41 +20,21 @@ Date::Date(int yy, int mm, int dd)
 }
 
 //void init_day(Date& dd, int y, int m, int d)
-void Date::add_day(Date& dd, int n)
+void Date::add_day(int n)
 {
-	dd.d += n;
-
-}
-//
-//void f()
-//{
-//	Date today {1978,6,25};
-	//Date tomorrow;
-
-	//init_day(today, 1987, 6, 25);
-
-	//today.y = {1978};
-	//today.m = {6};
-	//today.d = {25};
-
-	//tomorrow.d = add_day(today, 1);
-	//tomorrow.y = today.y;
-	//tomorrow.m = today.m;
-
-	//cout << today.y << '\n';
 	//
+}
 
-//}
 
 int main()
 {
 	//f();
 	Date today{1978,6,25};
-	Date tomorrow;
+	//Date tomorrow;
 
-	add_day(today, 1);
+	//add_day(today, 1);
 
-	cout << today.y << '\n';
+	cout << today.month() << '\n';
 
 
 }
