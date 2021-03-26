d0efd98f79a3a67398b71e12b287e9ba9fa7f3fa
debian <debian@debian-T4>
2021-03-26 22:04:17 +0100

    updates

--- 9hst/9.4.cpp
+++ 9hst/9.4.cpp
@@ -2,27 +2,41 @@
 
 class Date{
 	public: 
-	
+	class Invalid{};	
 	Date(int y, int m, int d);
 	void add_day(int n);
 	int year() {return y;}
 	int month() {return m;}
 	int day() {return d;}
-
 	private:
 	int y, m, d;
+	bool is_valid();
 };
 
 Date::Date(int yy, int mm, int dd)
 	:y{yy},m{mm},d{dd}
 {
 	//constructor
+	if(!is_valid()) throw Invalid{};
+}
+
+bool Date:is_valid()
+{
+	if(m<1||12<m) return false;
+
 }
 
 //void init_day(Date& dd, int y, int m, int d)
 void Date::add_day(int n)
 {
 	//
+}
+
+void f(int x, int y)
+try{
+	Date dxy {2004,x,y};
+	cout << dxy
+
 }
 
 
