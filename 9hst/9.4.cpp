fe077d14ecd787a57a3fea790a30170b39f0f5b0
debian <debian@debian-T4>
2021-03-26 18:51:08 +0100

    constructor

--- 9hst/9.4.cpp
+++ 9hst/9.4.cpp
@@ -1,29 +1,24 @@
 #include "../include/std_lib_facilities.h"
 
 struct Date {
-	int y;
-	int m;
-	int d;
+	int y, m, d;
+	Date(int y, int m, int d);
+	void add_day(int n);
 
 };
 
-void init_day(Date& dd, int y, int m, int d)
-{
-}
-
-void add_day(Date& dd, int n)
-{
-	dd.d += n;
+//void init_day(Date& dd, int y, int m, int d)
 
 
-}
+
+//void add_day(Date& dd, int n)
 
 int main()
 {
-	Date today;
+	Date today{1978, 6, 25};
 	//Date tomorrow;
 
-	init_day(today, 1987, 6, 25);
+	//init_day(today, 1987, 6, 25);
 
 	//today.y = {1978};
 	//today.m = {6};
