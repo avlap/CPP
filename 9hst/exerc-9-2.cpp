567920530af2bb111f3fabe4178deb9cc431fc23
debian <debian@debian-T4>
2021-03-28 11:34:03 +0200

    exercises

---
+++ 9hst/exerc-9-2.cpp
@@ -0,0 +1,29 @@
+#include "../include/std_lib_facilities.h"
+
+class Name_pairs {
+	public:
+	vector<string> name;
+	vector<double> age;
+	void read_names();
+	void read_ages();
+	void print();
+	void sort();
+
+};
+
+void Name_pairs::read_names()
+{
+	cout << "Enter names: " << '\n';
+	for(string tmp; cin>>tmp;)
+		name.push_back(tmp);
+}
+
+void Name_pairs::print()
+{
+}
+
+
+int main () 
+{
+
+}
