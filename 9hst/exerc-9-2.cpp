945d72098a7115be4ae7d34acd339adc2ad903f5
debian <debian@debian-T4>
2021-03-28 11:58:54 +0200

    step back

--- 9hst/exerc-9-2.cpp
+++ 9hst/exerc-9-2.cpp
@@ -3,29 +3,24 @@
 class Name_pairs {
 	public:
 	vector<string> name;
-	vector<double> age;
-	void read_names();
-	void read_ages();
-	void print();
-	void sort();
+	//vector<double> age;
+	
+	//void read_names(Name_pairs& np, string name);
+	//void read_ages();
+	//void print();
+	//void sort();
 
 };
 
-void Name_pairs::read_names()
-{
-	cout << "Enter names: " << '\n';
-	for(string tmp; cin>>tmp;)
-		name.push_back(tmp);
-}
 
-void Name_pairs::print()
-{
-	for(string x:name)
-		cout << x << '\n';
-}
 
 
 int main () 
 {
-
+	Name_pairs np;
+	np.name = {"dirk"};
+	cout << np.name[0] << '\n';
+	
 }
+
+
