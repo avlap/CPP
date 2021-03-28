9162b927d0a07f1f2972e065dd84cb8be6016522
debian <debian@debian-T4>
2021-03-28 12:19:12 +0200

    fix scope

--- 9hst/exerc-9-2.cpp
+++ 9hst/exerc-9-2.cpp
@@ -5,22 +5,39 @@ class Name_pairs {
 	vector<string> name;
 	vector<double> age;
 	
-	//void read_names(Name_pairs& np, string name);
+	void read_names(Name_pairs& np);
 	//void read_ages();
-	//void print();
+	void print();
 	//void sort();
 
 };
 
+void Name_pairs::read_names(Name_pairs& np)
+{
+	for(string tmp; cin>>tmp;)
+		np.name.push_back(tmp);
 
+}
+
+void Name_pairs::print()
+{
+	cout << "hallo! \n";
+}
+
+void f()
+{
+	//Name_pairs np;
+
+}
 
 
 int main () 
 {
-	Name_pairs np;
+	Name_pairs np;	
 	np.name = {"dirk"};
-	np.age = {18};
-	cout << np.name[0] << '\t' << np.age[0] << '\n';
+	np.print();
+	//np.age = {18};
+	//cout << np.name[0] << '\t' << np.age[0] << '\n';
 	
 }
 
