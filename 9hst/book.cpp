928ca3ce95a5f2c9b4fc41b06569196b38f38848
debian <debian@debian-T4>
2021-03-29 12:35:12 +0200

    title

--- 9hst/book.cpp
+++ 9hst/book.cpp
@@ -4,21 +4,35 @@ class Book {
 	//isbn, title, author, copyright-date. checked in or out,.
 	//
 	public:
-
 		bool checked = false;
 		void is_checked();
 		void check_in();
 		void check_out();
+		void set_title();
 
-		string title() {return t;}
+
+		string new_title(string n);
 		string author() {return a;}
 		string isbn() {return i;}
 	private:
 		string t;
 		string a;
 		string i;
-
 };
+
+void set_title()
+{
+
+}
+
+string Book::new_title(string n)
+{
+	t = n;
+	return t;
+
+	//cout << new_title(t) << '\n';
+
+}
 
 
 void Book::check_in()
@@ -29,6 +43,7 @@ void Book::check_in()
 
 void Book::check_out()
 {
+
 	checked = false;
 	//return checked;
 }
@@ -37,12 +52,15 @@ void Book::is_checked()
 	//return checked;
 	if(checked == true) cout << "checked in" << '\n';
 	else if (checked == false) cout << "checked out" << '\n';
+
 	else error("something wrong with the check");
 }
 
 int main()
 {
 	Book b;
+	string t = "Ik ga leven";
+	b.new_title(t);
 	b.is_checked();
 	b.check_in();
 	b.is_checked();
