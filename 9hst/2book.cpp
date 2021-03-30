4641f56e2a32e399fa0ed13547f48396cd2e5fca
debian <debian@debian-T4>
2021-03-30 10:30:00 +0200

    skip to 10

--- 9hst/2book.cpp
+++ 9hst/2book.cpp
@@ -2,85 +2,83 @@
 
 namespace Library {
 
-enum class Genre {
-	fiction, nonfiction, thriller, children
-};
+	enum class Genre {
+		fiction, nonfiction, thriller, children
+	};
 
-class Book {
-	//isbn, title, author, copyright-date. checked in or out,.
-	public:
-		Book(string i);
-		class Invalid{};
+	class Book {
+		//isbn, title, author, copyright-date. checked in or out,.
+		public:
+			Book(string i);
+			class Invalid{};
 
-		void is_checked() { cout << "checked: " << checked << '\n'; }
-		void check_in() { checked = true; }
-		void check_out() { checked = false; }
-		void print_isbn() { cout << isbn() << '\n'; }
+			void is_checked() { cout << "checked: " << checked << '\n'; }
+			void check_in() { checked = true; }
+			void check_out() { checked = false; }
+			void print_isbn() { cout << isbn() << '\n'; }
 
-		string author() { return a; }
-		string isbn() { return i; }
-	private:
-		//	string t;
-		string a;
-		string i;
-		bool checked = false;
+			string author() { return a; }
+			string isbn() { return i; }
+		private:
+			//	string t;
+			string a;
+			string i;
+			bool checked = false;
 
-		bool is_isbn_valid();
+			bool is_isbn_valid();
+	};
 
-};
-
-Book::Book(string ii)
-	:i{ii}
-{
-	//check for valid
-	if(!is_isbn_valid()) throw Invalid{};
-
-}
-
-bool Book::is_isbn_valid()
-{
-	//isbn should be a string
-	//n-n-n-x, where n is integer and x is a digit or a letter
-	//Tokenize?
-	for(int j=0; j<=4; j+=2) {
-		if(!isdigit(i[j])) return false; 
+	Book::Book(string ii)
+		:i{ii}
+	{
+		//check for valid
+		if(!is_isbn_valid()) throw Invalid{};
 	}
 
-	for(int j = 1; j <= 5; j+=2) {
-		if(i[j] != '-') return false; 
+	bool Book::is_isbn_valid()
+	{
+		//isbn should be a string
+		//n-n-n-x, where n is integer and x is a digit or a letter
+		//Tokenize?
+		for(int j=0; j<=4; j+=2) {
+			if(!isdigit(i[j])) return false; 
+		}
+
+		for(int j = 1; j <= 5; j+=2) {
+			if(i[j] != '-') return false; 
+		}
+
+		if(isdigit(i[i.length() -1]) || isalpha(i[i.length() -1])) {
+			//do nothing
+		} else { 
+			return false; 
+		}
+
+		return true;
 	}
 
-	if(isdigit(i[i.length() -1]) || isalpha(i[i.length() -1])) {
-		//do nothing
-	} else { 
-		return false; 
+	bool operator==(Book& a, Book& b)
+	{
+		return a.isbn()==b.isbn();
 	}
 
-	return true;
-}
+	bool operator!=(Book& a, Book& b)
+	{
+		return a.isbn()!=b.isbn();
+	}
 
-bool operator==(Book& a, Book& b)
-{
-	return a.isbn()==b.isbn();
-}
+	ostream& operator<<(ostream& os, Book& a)
+	{
+		return os << "ISBN: " << a.isbn() << '\n';
+	}
 
-bool operator!=(Book& a, Book& b)
-{
-	return a.isbn()!=b.isbn();
-}
-
-ostream& operator<<(ostream& os, Book& a)
-{
-	return os << "ISBN: " << a.isbn() << '\n';
-}
-
-} //Library
+} //namespace Library
 
 using namespace Library; 
 
 int main()
 	try{
-				Book a("0-0-0-0"); //n-n-n-x,
+		Book a("0-0-0-0"); //n-n-n-x,
 		Book b("0-0-0-n");
 		if(a!=b)  
 			cout << "a != b" << '\n';
