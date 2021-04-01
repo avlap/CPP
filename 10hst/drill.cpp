154267ab3b3058f4ba10f782f3bf04228b335d82
d <you@example.com>
2021-04-01 20:55:12 +0200

    drill

--- 10hst/drill.cpp
+++ 10hst/drill.cpp
@@ -7,23 +7,34 @@ struct Point {
 };
 
 
-void fill_point()
+void fill_point(vector<Point>& orig_p)
 {
-	vector<Point>original_points;
 	int x;
 	int y;
-	
+
 	cout << "Give us 2 points: \n";
 	for(int i=0; i<2; ++i) {
-	for(Point p; cin>> x >>y;)
-		original_points.push_back(Point{x,y});
+		for(Point p; cin>> x >>y;)
+			orig_p.push_back(Point{x,y});
 	}
 
 }
 
+void print_point(const vector<Point>& p)
+{
+	for(Point z : p)
+		cout << z.x << '\t' << z.y << '\n';
+
+}
+
+
+
+
 int main()
 {
+	vector<Point>original_points;
 
-	fill_point();
-	
+	fill_point(original_points);
+	print_point(original_points);
+
 }
