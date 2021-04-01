552b5de2887ea10c01a298258dfbf96dff0638c8
d <you@example.com>
2021-04-01 21:47:59 +0200

    print to file

--- 10hst/drill.cpp
+++ 10hst/drill.cpp
@@ -8,27 +8,36 @@ struct Point {
 
 void fill_point(vector<Point>& orig_p)
 {
-
 	cout << "Give us 7 points: \n";
-		for(Point p; cin>> p.x >>p.y;)
+	while (cin) {
+		for(Point p; cin>> p.x >>p.y;) 
 			orig_p.push_back(Point{p.x,p.y});
-	
-
+	}
 }
 
-void print_point(const vector<Point>& p)
+void print_point_cout(const vector<Point>& p)
 {
 	for(Point z : p)
 		cout << z.x << '\t' << z.y << '\n';
-
 }
 
+void print_point_file(const vector<Point>& p, string name)
+{
+	//string name;
+	//cout << "please enter name output file: \n";
+	//cin >> oname;
+	ofstream ost{name}; //ost write to a file named oname
+//	if (!ost) error("can't open output file ", oname);
+	for(Point z:p)
+		ost << z.x << '\t' << z.y << '\n';
+}
 
 int main()
 {
+	string s = "out_file";
 	vector<Point>original_points;
 
 	fill_point(original_points);
-	print_point(original_points);
-
+	//print_point_cout(original_points);
+	print_point_file(original_points, s);
 }
