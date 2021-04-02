342a320d743226e3b72d06c474e429fbd425cab1
d <you@example.com>
2021-04-02 14:50:07 +0200

    sum function

--- 10hst/1.10.cpp
+++ 10hst/1.10.cpp
@@ -5,9 +5,6 @@
 //report errors
 //add integers to a vector?
 //function which calculate the sum
-//
-//
-
 
 void fill_vector(istream& ist, vector<int>& v, char terminator)
 	//read integers from ist into v until we reach eof() or terminator
@@ -22,7 +19,6 @@ void fill_vector(istream& ist, vector<int>& v, char terminator)
 
 	char c;
 	ist>>c; //read a character, hopefully terminator
-
 	if(c != terminator) {
 		ist.unget();
 		ist.clear(ios_base::failbit); // set the state to fail()
@@ -30,19 +26,24 @@ void fill_vector(istream& ist, vector<int>& v, char terminator)
 
 }
 
-void print_vector(const vector<int>& v)
+int calculate_sum(const vector<int>& v)
 {
-	for(int x:v)
-		cout << x << '\n';
+	int sum=0;
+	for(int x:v) sum += x;
+
+	return sum;
 
 }
 
 int main()
 {
+	int sum;
 	vector<int>numbers;
 	string iname = "integers";
+
 	ifstream ist {iname};
 	fill_vector(ist, numbers, '|');
-	print_vector(numbers);
-
+	//print_vector(numbers);
+	sum = calculate_sum(numbers);
+	cout << "Sum: " << sum << '\n';
 }
