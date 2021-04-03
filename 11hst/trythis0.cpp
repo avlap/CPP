f62648891269b7996d42e3f3a2b7afeccaf1b5b8
d <you@example.com>
2021-04-03 21:20:22 +0200

    setw(8)

---
+++ 11hst/trythis0.cpp
@@ -0,0 +1,15 @@
+#include "../include/std_lib_facilities.h"
+
+void print_column()
+{
+	cout << '|' << setw(8) << "Henk" << '|'<< "Oosterhuis" << '|'<< "20-02-1977" << "| \n";
+
+}
+
+int main()
+{
+
+	print_column();
+//	cout << "moi" << '\n';
+
+}
