cac02f2cfb0180a788d510cb499a01c6af4d940d
d <you@example.com>
2021-04-07 16:05:46 +0200

    12.7.4

--- fltk/12hst/drill0.cpp
+++ fltk/12hst/drill0.cpp
@@ -5,39 +5,25 @@ int main()
 {
 	//y_max-bottom_margin
     using namespace Graph_lib;
-   
+
     Point tl {100,100};
-   
+
     Simple_window win {tl,600,400,"Canvas"};
+
+	Function sine {sin,0,100,Point{20,150},1000,50,50};
 
 	//axis
 
-	Axis xa {Axis::x, Point{100,300},280,10,"x axis"};
-
+	Axis xa {Axis::x, Point{20,300},280,10,"x axis"};
 	Axis ya {Axis::y, Point{20,300},280,10,"y axis"};
+
 	ya.set_color(Color::cyan);
 	ya.label.set_color(Color::dark_red);
-		//win.set_label("Canvas #3");
-	//win.wait_for_button();
-
 	
-   
-  //polygon 
-    Polygon poly;
-   
-    poly.add(Point{300,200});
-    poly.add(Point{350,100});
-    poly.add(Point{400,200});
-   
-    poly.set_color(Color::red);
-
-	
+	win.attach(sine);
+	win.set_label("Canvas #4");
   	win.attach(ya);
-
 	win.attach(xa);
 	win.set_label("VANVAS");
-
-    win.attach (poly);
-   
     win.wait_for_button();
 }
