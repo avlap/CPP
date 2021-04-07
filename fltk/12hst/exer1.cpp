e6d0ac88331e6c3ecc539c5f5c1de487e741e551
d <you@example.com>
2021-04-07 18:50:12 +0200

    polygon rectangle

--- fltk/12hst/exer1.cpp
+++ fltk/12hst/exer1.cpp
@@ -11,8 +11,21 @@ int main()
 	Simple_window win {tl,600,400,"Canvas"};
 
 	Rectangle r {Point{200,200},100,50};
+	r.set_color(Color::blue);
 	win.attach(r);
+
 	win.set_label("Rectangle");
+
+	Polygon poly;
+	poly.add(Point{100,100});
+	poly.add(Point{200,100});
+	poly.add(Point{100,200});
+	poly.add(Point{200,200});
+
+	poly.set_color(Color::yellow);
+
+	win.attach(poly);
+
 
 	win.wait_for_button();
 
