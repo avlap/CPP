a1a0c38e12ac9e181bdeded46f8dbbad0cc30aae
d <you@example.com>
2021-04-07 17:48:44 +0200

    fill

--- fltk/12hst/drill0.cpp
+++ fltk/12hst/drill0.cpp
@@ -27,16 +27,23 @@ int main()
 
 	Polygon poly;
 	poly.add(Point{300,200});
-	poly.add(Point{350,100});
+	poly.add(Point{350,10});
 	poly.add(Point{400,200});
 
 	poly.set_color(Color::red);
 	poly.set_style(Line_style::dash);
 	win.attach(poly);
-	win.set_label("Canvas #5");
 
 
-	
+	Rectangle r {Point{200,200},100,50};
+	win.attach(r);
+
+	r.set_fill_color(Color::yellow);
+	poly.set_style(Line_style(Line_style::dash,8));
+//	poly_rect.set_style(Line_style(Line_style::dash,2));
+//	poly_rect.set_fill_color(Color::green);
+
+	win.set_label("Canvas #7");
 	//	win.set_label("Canvas #4");
     win.wait_for_button();
 }
