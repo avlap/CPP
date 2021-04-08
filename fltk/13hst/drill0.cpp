49aeab2609e5fc3d04b08dac759c1647d868af9c
d <you@example.com>
2021-04-08 18:53:36 +0200

    rectangles

--- fltk/13hst/drill0.cpp
+++ fltk/13hst/drill0.cpp
@@ -22,12 +22,23 @@ int main()
 	Lines grid;
 	for(int x=x_grid;x <(x_size-x_grid); x+=x_grid)
 		grid.add(Point{x,0},Point{x,x_size});
+
 	for(int y=y_grid;y<(y_size-y_grid);y+=y_grid)
 		grid.add(Point{0,y},Point{x_size,y});
 
 	win.attach(grid);
 
+	//Rectangle rect;
 	
+	//Rectangle rect (Point{i, i}, Point{x_grid+i, y_grid+i});
+	Vector_ref<Rectangle> vr;
+
+	for(int i=0; i<(x_size-x_grid); i+=x_grid) {
+		vr.push_back(new Rectangle(Point{i, i}, Point{x_grid +i, y_grid + i}));
+		vr[vr.size()-1].set_fill_color(Color::yellow);
+		win.attach(vr[vr.size()-1]);
+		}
+
 	win.wait_for_button();
 
 }
