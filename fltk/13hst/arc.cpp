e887267853f0083318f4bdb0fd29c60ca31afb9d
d <you@example.com>
2021-04-10 17:20:13 +0200

    arc private

--- fltk/13hst/arc.cpp
+++ fltk/13hst/arc.cpp
@@ -74,24 +74,25 @@ void draw_arc() const
 */
 //copy from Ellipse
 struct Arc : Shape {
-	Arc(Point p, int w, int h, double a, double b);
+	Arc(int x, int y, int w, int h, double a, double b);
 
 //	void draw_lines() const;
 void draw_lines() const
 {
 	if(color().visibility())
-		fl_arc(point(0).x, point(0).y,w,h,a,b);
+		fl_arc(x,y,w,h,a,b);
 }
-//	private:
-	//int x, y;
+
+	private:
+	int x, y;
 	int w;
 	int h;
 	double a;
 	double b;
 };
 
-Arc::Arc(Point p, int ww, int hh, double aa, double bb)
-	:w{ww},h{hh},a{aa},b{bb}
+Arc::Arc(int xx, int yy, int ww, int hh, double aa, double bb)
+	:x{xx},y{yy},w{ww},h{hh},a{aa},b{bb}
 {
 	if(b<a) error("a larger then b");
 }
@@ -106,7 +107,7 @@ int main()
 
 	Simple_window win {tl,600,400,"Canvas"};
 
-	Arc a1 {Point{100,200},50,60,0.0,333};
+	Arc a1 {100,200,50,60,0.0,333};
 
 	win.attach(a1);
 
