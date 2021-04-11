0bee4675ce0253621cbc3360e9cff2ad2a865237
d <you@example.com>
2021-04-11 20:30:47 +0200

    north north-west

--- fltk/13hst/functions_rect.cpp
+++ fltk/13hst/functions_rect.cpp
@@ -1,38 +1,45 @@
 #include "Simple_window.h"
 #include "Graph.h"
-Point print_point(const Rectangle& r)
+Point north_west(const Rectangle& r)
 {
 	//cout << "Point{" << r.point(0).x << ',' << r.point(0).y << "}\n";
-	Point p {r.point(0).x + 50, r.point(0).y + 50};
-
-//	if(r.point(0).x > 20) error("groter dan 0");
+	Point p {r.point(0).x, r.point(0).y};
 
 	return p;
 }
 
-int main()
-	try
+Point north(const Rectangle& r)
 {
-		using namespace Graph_lib;
+	Point p {r.point(0).x + (r.width()/2), r.point(0).y};
 
-	Point tl{100,100};
-
-	Simple_window win {tl,600,400,"Canvas"};
-
-	Rectangle r {Point{150,100}, 200, 100};
-
-	Point p;
-
-	p = print_point(r);
-	//	cout << p.point(0).x << '\n';
-	Rectangle r1 {p, 200, 100};
-
-	win.attach(r);
-	win.attach(r1);
-
-	win.wait_for_button();
+	return p;
 
 }
+
+int main()
+	try {
+		using namespace Graph_lib;
+
+		Point tl{100,100};
+
+		Simple_window win {tl,600,400,"Canvas"};
+
+		Rectangle r {Point{150,100}, 200, 100};
+
+		Point pnw;
+		Point pn;
+
+		pnw = {north_west(r)};
+		pn = {north(r)};
+		//	cout << p.point(0).x << '\n';
+		Rectangle rnw {pnw, 200, 100};
+		Rectangle rn{pn, 100, 100};
+
+		win.attach(rn);
+		win.attach(rnw);
+
+		win.wait_for_button();
+	} 
 catch(exception& e) {
 	// some error reporting
 	return 1;
