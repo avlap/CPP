8575f909dc25b2bf66490a09304065de39ad56eb
d <you@example.com>
2021-04-11 12:36:40 +0200

    box from scratch

--- fltk/13hst/box.cpp
+++ fltk/13hst/box.cpp
@@ -1,18 +1,17 @@
-//#include "fltk.h"
 #include "Simple_window.h"
 #include "Graph.h"
-/*
-/copy from Ellipse
+
 struct Arc : Shape {
+	Arc(Point p, int w, int h, double a, double b);
 	Arc(int x, int y, int w, int h, double a, double b);
 
-//	void draw_lines() const;
 void draw_lines() const
 {
 	if(color().visibility())
 		fl_arc(x,y,w,h,a,b);
 }
-//	private:
+
+	private:
 	int x, y;
 	int w;
 	int h;
@@ -25,7 +24,18 @@ Arc::Arc(int xx, int yy, int ww, int hh, double aa, double bb)
 {
 	if(b<a) error("a larger then b");
 }
-*/
+
+Arc::Arc(Point p, int ww, int hh, double aa, double bb)
+	:x{p.x},y{p.y},w{ww},h{hh},a{aa},b{bb}
+{
+	if(b<a) error("a larger then b");
+	add(p);
+}
+
+//box, 4 lines, 4 arcs.
+//Try to make a box, with one full arc first
+
+//struct Box : Shape
 
 int main()
 {
@@ -33,19 +43,19 @@ int main()
 	using namespace Graph_lib;
 
 	Point tl{100,100};
+	Point bx{200,220};
 
 	Simple_window win {tl,600,400,"Canvas"};
 
-	//Arc a1 {100,200,50,60,0.0,333};
+	Rectangle rect0 {bx,50,60};
 
-	//win.attach(a1);
+	Arc a1 {bx,50,60,0.0,333.0};
+	//Arc a2 {300, 100, 70, 80, 22.0, 229.0};
+	//Arc a2 {100,220,50,60,0.0,333.0};
 
-
-
-
-
-	//fl_arc
-
+	win.attach(rect0);
+	win.attach(a1);
+	//win.attach(a2);
 
 	win.wait_for_button();
 
