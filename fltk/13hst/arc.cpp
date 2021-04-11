2fbbc5284bcaea6e7888681825c41d9b7cca3bda
d <you@example.com>
2021-04-11 12:29:14 +0200

    bit cleanup

--- fltk/13hst/arc.cpp
+++ fltk/13hst/arc.cpp
@@ -1,86 +1,9 @@
-//#include "fltk.h"
 #include "Simple_window.h"
 #include "Graph.h"
 
-/*
-struct Circle : Shape { //Circle has Shape as base
-	Circle(Point p, int rr); //center and radius
-
-	void draw_lines() const; //const it can't be changed
-
-	Point center() const; //center is a function
-	int radius() const { return r; } //function radius is a int and returns r;
-	void set_radius(int rr) //not only declaration but also statement
-	{
-		set_point(0,Point{center().x-rr,center().y-rr}); //maintain the center
-
-		r = rr; //r is private
-	}
-	private:
-		int r;
-};
-
-*/
-
-/*
-void fl_arc(int	x, int y, int w, int h, double a1, double a2 )
-	 
-*/
-//can we use functions of Ellipse? Just a other constructer and additions?
-/*
-struct Arc : Shape {
-	Arc(Point p, int w, int h, double a1, double a2); //center, max and min distance from center. Degrees for a1 and a2. A2 should be larger then a1 (function to check?);
-
-	//copy from Ellipse class
-	//void draw_lines() const;
-void draw_arc() const
-{
-	if(color().visibility())
-		fl_arc(point(0).x,point(0).y,w,h,a1,a2);
-
-}
-
-	Point center() const;
-//	Point focus1() const;
-//	Point focus2() const;
-
-	void set_major(int ww)
-	{
-		set_point(0,Point{center().x-ww,center().y-h}); //maintain the center, copied
-
-		w = ww;
-	}
-
-	int major() const { return w; }
-
-	void set_minor(int hh)
-	{
-		set_point(0,Point{center().x-w,center().y-hh}); //maintain te center copied
-
-		h = hh;
-	}
-
-	int degrees_a() const { return a1; }
-	int degrees_b() const { return a2; }
-
-	//void operator>=a2_larger_a1 //pseudocode
-
-	private:
-		int w;
-		int h;
-		double a1;
-		double a2;
-};
-*/
-//copy from Ellipse
 struct Arc : Shape {
 	Arc(Point p, int w, int h, double a, double b);
 	Arc(int x, int y, int w, int h, double a, double b);
-
-
-
-//	void draw_lines() const;
-//Point center() const { return p; }
 
 void draw_lines() const
 {
@@ -109,9 +32,6 @@ Arc::Arc(Point p, int ww, int hh, double aa, double bb)
 	add(p);
 }
 
-
-
-
 int main()
 {
 	//y_max-bottom_margin
@@ -127,12 +47,6 @@ int main()
 
 	win.attach(a1);
 	win.attach(a2);
-
-
-
-
-	//fl_arc
-
 
 	win.wait_for_button();
 
