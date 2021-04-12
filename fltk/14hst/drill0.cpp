68f35cd84905bafda1efcdd536e54265fbaf7cfe
d <you@example.com>
2021-04-12 16:42:35 +0200

    B

---
+++ fltk/14hst/drill0.cpp
@@ -0,0 +1,39 @@
+#include "Simple_window.h"
+#include "Graph.h"
+
+struct B1 : Shape {
+	B1(Point p1, Point p2);
+	//virtual void vf();
+	//void f();
+
+	//void draw_lines() const;
+
+
+
+};
+
+
+B1::B1(Point p1, Point p2)
+{
+	add(p1);
+	add(p2);
+}
+/*
+void B1::draw_lines() const
+{
+	Line(p1, p2);
+}
+*/
+int main()
+{
+	Point tl{100,100};
+	Simple_window win1 {tl,600,400,"two lines"};
+	B1 b0 {Point{100,100}, Point{100,50}};
+
+	win1.attach(b0);
+
+
+	win1.wait_for_button();
+
+}
+
