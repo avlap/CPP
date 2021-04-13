844dc077359c707d86ca71596b77cf92dff89c11
d <you@example.com>
2021-04-13 13:39:32 +0200

    example

--- fltk/16hst/example.cpp
+++ fltk/16hst/example.cpp
@@ -1,7 +1,57 @@
 #include "Simple_window.h"
 #include "Graph.h"
 
+struct Lines_window : Window {
+	Lines_window(Point xy, int w, int h, const string& title);
+	Open_polyline lines;
+private:
+	Button next_button; //add (next_x next_y) to lines
+	Button quit_button;
+	In_box next_x;
+	In_box next_y;
+	Out_box xy_out;
 
+	void next();
+	void quit();
+};
+
+//Lines_window constructor, with lambdas
+Lines_window::Lines_window(Point xy, int w, int h, const string& title)
+	:Window{xy,w,h,title},
+	next_button{Point{x_max()-150, 0}, 70, 20, "Next point", [](Address, Address pw) {reference_to<Lines_window>(pw).next();}},
+	quit_button{Point{x_max()-70,0}, 70, 20, "Quit", [](Address, Address pw) {reference_to<Lines_window>(pw).quit();}},
+	next_x{Point{x_max()-310,0}, 50, 20, "Next x:"},
+	next_y{Point{x_max()-210,0},50,20,"next y:"},
+	xy_out{Point{100,0}, 100, 20. "current (x,y):"}
+		{
+			attach(next_button);
+			attach(quit_button);
+			attach(next_x);
+			attach(next_y);
+			attach(xy_out);
+			attach(lines);
+
+		}
+
+void Lines_window::quit()
+{
+	hide();
+}
+
+void Lines_window::next()
+{
+	int x = next_x.get_int();
+	int y = next_y.get_int();
+
+	lines.add(Point{x,y});
+
+	//update current position readout
+	ostringsttream ss;
+	ss << '(' << x << ',' << y << ')';
+	xy_out.put(ss.str());
+
+	redraw();
+}
 
 int main()
 {
