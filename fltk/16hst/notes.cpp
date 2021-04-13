f10904eb6c473a99ab5bd3e946c6fa8d6cd32817
d <you@example.com>
2021-04-13 12:53:08 +0200

    notess

--- fltk/16hst/notes.cpp
+++ fltk/16hst/notes.cpp
@@ -22,6 +22,29 @@ Simple_window::Simple_window(Point xy, int w, int h, const string& title)
 
 	}
 
+void Simple_window::cb_next(Address, Address pw)
+{
+	reference_to<Simple_window>(pw).next();
+
+}
+
+void Simple_window::wait_for_button()
+{
+	while(!button_pushed)FL::wait();
+	button_pushed = false;
+	FL::redraw();
+}
+
+class Button : public Widget{
+	public:
+		Button(Point xy, int ww, int hh, const string& s, Callback cb)
+			:Widget{xy,ww,hh,s,cb} {}
+
+		void attach(Window& win);
+};
+
+//unsigned can only hold positive integers
+
 int main()
 {
 }
