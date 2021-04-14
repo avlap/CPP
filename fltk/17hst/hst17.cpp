5a5cbe412b92d1151dd7dc4d3733de9b43251ed9
d <you@example.com>
2021-04-14 21:41:34 +0200

    sizeof()

---
+++ fltk/17hst/hst17.cpp
@@ -0,0 +1,18 @@
+#include "Simple_window.h"
+#include "Graph.h"
+
+void sizes(char ch, int i, int* p)
+{
+	cout << "size char" << sizeof(char) << ' ' << sizeof(ch) << '\n';
+	cout << "size char" << sizeof(int) << ' ' << sizeof(i) << '\n';
+	cout << "size char" << sizeof(int*) << ' ' << sizeof(p) << '\n';
+}
+
+int main()
+{
+	char c = 'c';
+	int i = 8;
+	int* p = &i;
+	sizes(c, i, p);
+}
+
