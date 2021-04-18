68e6c60529ba6da6289e6cf268990a0a1ec4d2b0
d <you@example.com>
2021-04-18 13:48:54 +0200

    Sales_item.h

--- Primer/test.cpp
+++ Primer/test.cpp
@@ -4,13 +4,9 @@ using namespace std;
 
 int main()
 {
-	//int sum = 50;
-	int i = 10;	
-	while(i >= 0) {
-		cout << i << '\n'; 
-		--i;
-	}
 
+	for(int i = 0; i <= 10; ++i) 
+		cout << i << '\n';
 
 	return 0;
 
